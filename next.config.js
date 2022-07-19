df29ca0072181e671db5e7418a4a01e1ccb5ebaf
jeong-mi <wjdal6951@naver.com>
2022-07-20 00:57:12 +0900

    feat: request를 masking함으로써 api key 숨기기!

--- next.config.js
+++ next.config.js
@@ -1,7 +1,27 @@
 /** @type {import('next').NextConfig} */
+
+const API_KEY = "3f1c334f9556d8465b9592eec2879ef0";
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/contact",
+        destination: "https://",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
