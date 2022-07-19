dd2d3c0e09f23dee0a6429f312efbef70ac527eb
jeong-mi <wjdal6951@naver.com>
2022-07-20 00:38:50 +0900

    feat: TMDB에서 get api 사용

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
+const API_KEY = "3f1c334f9556d8465b9592eec2879ef0";
+
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
   return (
     <>
       <Seo title="About" />
-      <h1 className="active">Hello</h1>
+      {!movies && <h4>Loading..</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </>
   );
 }
